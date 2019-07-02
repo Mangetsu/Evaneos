a2fad57c82fb524478d95a8d3340da655e73d0ef
Badr HAKKARI <badr.hakkari@gmail.com>
2019-07-02 15:06:29 +0200

    TemplateManager Refactoring (Removing the inecessary methods and code) Applying the PHPCS recommendations

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -2,72 +2,154 @@
 
 class TemplateManager
 {
-    public function getTemplateComputed(Template $tpl, array $data)
-    {
+    /**
+     * @var ApplicationContext
+     */
+    private $context;
+
+    /**
+     * @var Quote
+     */
+    private $_quote;
+
+    /**
+     * @var Destination
+     */
+    private $_destination;
+
+    /**
+     * @var User
+     */
+    private $_user;
+
+    /**
+     * @var Site
+     */
+    private $_site;
+
+    /**
+     * @param Template $tpl
+     * @param array $data
+     * @return Template
+     */
+    public function getTemplateComputed(Template $tpl, array $data) {
         if (!$tpl) {
             throw new \RuntimeException('no tpl given');
         }
 
+        $this->initTemplateObjects($data);
+
         $replaced = clone($tpl);
-        $replaced->subject = $this->computeText($replaced->subject, $data);
-        $replaced->content = $this->computeText($replaced->content, $data);
+        $replaced->subject = $this->_computeText($replaced->subject, $data);
+        $replaced->content = $this->_computeText($replaced->content, $data);
 
         return $replaced;
     }
 
-    private function computeText($text, array $data)
-    {
-        $APPLICATION_CONTEXT = ApplicationContext::getInstance();
+    private function _computeText($text) {
+        preg_match_all("/\[[^\]]*\]/", $text, $matches);
 
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
-
-        if ($quote)
-        {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
-
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
-        }
-
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
-
-        /*
-         * USER
-         * [user:*]
-         */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
-        if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
+        foreach ($matches[0] as $match) {
+            $text = str_replace($match, $this->getFieldValue($match), $text);
         }
 
         return $text;
+    }
+
+    /**
+     * Init all objects needed by a TemplateManager instance
+     *
+     * @param $data array Data Array used as a parameter bag (From getTemplateComputed)
+     */
+    private function initTemplateObjects($data) {
+        $this->context = ApplicationContext::getInstance();
+
+        if (!(isset($data['quote']) || !($data['quote'] instanceof Quote))) {
+            throw new \RuntimeException('no object of type Quote is found in arguments');
+        }
+
+        $this->_user        = (isset($data['user']) and ($data['user'] instanceof User)) ? $data['user'] : $this->context->getCurrentUser();
+        $this->_quote       = $data['quote'];
+        $this->_site        = SiteRepository::getInstance()->getById($this->_quote->siteId);
+        $this->_destination = DestinationRepository::getInstance()->getById($this->_quote->destinationId);
+    }
+
+    /**
+     * Function to map the placeholder to a getter method,
+     * this will allow a dynamic replacement of the placeholder string from the text
+     *
+     * for each placeholder (ex: [quote:destination_link]) a private function with a camelCase name must be created
+     * inside the TemplateManager(ex: getQuoteDestinationLink() ) this method will call it dynamically
+     *
+     *
+     * @param $placeholder string   The placeholder text used in the template text
+     * @return string
+     */
+    private function getFieldValue($placeholder) {
+        $placeholder = str_replace(['[', ']'], '', $placeholder);
+        $field = explode(':', $placeholder);
+        $getter = 'get'.$field[0] . str_replace('_', '', ucwords($field[1], '_'));
+
+        return $this->$getter();
+    }
+
+    //Getters and Entity Association mapping
+    //TODO: Update Entities with mapped associations instead of object ID and move Getters to entities or a service
+
+    /**
+     * Returns the Quote Summary string in HTML
+     *
+     * @return string
+     */
+    private function getQuoteSummaryHtml() {
+        return $this->renderHtml($this->_quote->id);
+    }
+
+    /**
+     * Returns the Quote Summary as a simple string
+     *
+     * @return string
+     */
+    private function getQuoteSummary() {
+        return $this->_quote->id;
+    }
+
+    /**
+     * Return the user firstname as string
+     *
+     * @return string
+     */
+    private function getUserFirstName() {
+        return ucfirst(mb_strtolower($this->_user->firstname));
+    }
+
+    /**
+     * Create the destination link as string
+     *
+     * @return string
+     */
+    private function getQuoteDestinationLink() {
+        $link = $this->_site->url . '/' . $this->_destination->countryName . '/quote/' . $this->_quote->id;
+        return $link;
+    }
+
+    /**
+     * Return the destination name as string
+     *
+     * @return string
+     */
+    private function getQuoteDestinationName() {
+        return $this->_destination->countryName;
+    }
+
+    /**
+     * Renders a string inside an HTML paragraph tag
+     *
+     * @param $string
+     * @return string
+     */
+    private function renderHtml($string)
+    {
+        return '<p>'.$string.'</p>';
     }
 }
